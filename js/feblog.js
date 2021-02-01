0028b4667428e9d1a30075187f6fe8c113cc4d62
fractionofadot <parkerbobby@gmail.com>
2021-01-31 16:46:49 -0800

    Added new blog files and js

---
+++ js/feblog.js
@@ -0,0 +1,40 @@
+function displayPost() {
+	let params = (new URL(document.location)).searchParams;
+	let post_id = parseInt(params.get('post_id')); 
+
+	var converter = new showdown.Converter();
+
+	if (post_id > 0) { 
+		let post_file = 'posts/' + post_id + '.md';
+		fetch(post_file)
+			.then(response => {
+				if (response.ok) {
+					return response.text();
+				} else {
+					document.getElementById("post").innerHTML = '<p>Post not found.</p>';
+					throw new Error('Post not found');
+				}
+			})
+			.then(data => document.getElementById("post").innerHTML = converter.makeHtml(data)
+		);
+	}
+}
+
+function listPosts(postObject, fileName) {
+	let fileName = 'posts/index.json';
+	fetch(fileName)
+		.then(response => {
+			return response.json();
+		})
+		.then(data => {
+			for (var i = data.length - 1; i >= 0; i--) {
+				var node = document.createElement("article");
+				node.id = 'post-' + i;
+				node.className = "post-preview";
+				node.innerHTML = '<h2>' + data[i].title + '</h2><p>' + data[i].abstract + '</p>';
+				postObject.appendChild(node);
+			}
+	  }
+	);
+}
+
